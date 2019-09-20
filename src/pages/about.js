e8bc1788e43c8b1126f9935ce9c476a5cc4777b9
Katie Rose <katiefelten@gmail.com>
2019-09-20 15:14:50 -0400

    :zap: added basic stylings

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
   <>
     <h1>About Me</h1>
     <p>This is my site.</p>
     <Link to="/">&larr; back to home</Link>
+    <Layout />
   </>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
   <>
     <h1>Home</h1>
     <p>Hello friends</p>
     <Link to="/about">&larr; back to about me</Link>
+    <Layout />
   </>
 );
