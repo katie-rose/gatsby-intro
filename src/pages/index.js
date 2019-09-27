888e9ba2cfd8d6cd4c2c41c4f87843a57417b428
Katie Rose <katiefelten@gmail.com>
2019-09-27 12:50:05 -0400

    :zap: updated the index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,19 +4,19 @@ import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 import ReadLink from '../components/read-link';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Atlanta!</p>
-      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
