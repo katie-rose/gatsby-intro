35615266892e496e2825bca49e3ddec3422eb2f7
Katie Rose <katiefelten@gmail.com>
2019-09-27 12:20:51 -0400

    :zap: called posts via gatsby node and styled the pages slightly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
+import ReadLink from '../components/read-link';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Atlanta!</p>
-    <Link to="/about/">Learn about me &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Atlanta!</p>
+      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
